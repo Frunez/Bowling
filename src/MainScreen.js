beaa82b5f62c50fba20a9d602edb144855e477e7
Francesco Nunez <francesco.nunez@gmail.com>
2017-01-26 21:37:31 +0000

    Lots of fun styling stuff for a more immersive experience

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,15 +1,24 @@
-var bowling = new Bowling();
-var scoreBoard = new ScoreBoard();
-var rollScore = 0;
-var scoreString = '0';
-// function updateScore() {
-//   scoreBoard.frame.forEach(function(score) {
-//     return score;
-//   });
-// }
+$(document).ready(function() {
+    var bowling = new Bowling();
+    var scoreBoard = new ScoreBoard();
+    var rollScore = 0;
+    var scoreString = '0';
+    var timeOut = 0
+    // function updateScore() {
+    //   scoreBoard.frame.forEach(function(score) {
+    //     return score;
+    //   });
+    // }
 
-$('#roll').click(function() {
-  rollScore = bowling.roll();
-  scoreString = scoreBoard.getScore(rollScore);
-  $('#score').text(scoreString);
+    $('#roll').click(function() {
+        $('#roll').hide();
+        $('.ball').attr('class', 'rolling');
+        timeOut = setTimeout(function() {
+            $('.rolling').attr('class', 'ball');
+            rollScore = bowling.roll();
+            scoreString = scoreBoard.getScore(rollScore);
+            $('#score').text(scoreString);
+            $('#roll').show();
+        }, 5000);
+    });
 });
